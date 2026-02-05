5f0c8a9f28d6f59d5a48b344587294720c2bb892
Ashime Etunyi <ashimeetunyi@Ashimes-MacBook-Air.local>
2026-02-04 23:38:02 -0500

    updates

--- aws-infra/main.tf
+++ aws-infra/main.tf
@@ -2,7 +2,13 @@ resource "aws_s3_bucket" "keren-example-bucket" {
   bucket = "my-tf-test-bucket-for-keren-123456"
 
   tags = {
-    Name        = "Environment"
+    Name        = "keren-example-bucket-01"
     Environment = "Dev"
+  }
+}
+resource "aws_s3_bucket_versioning" "keren_example-versioning" {
+  bucket = aws_s3_bucket.keren-example-bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
